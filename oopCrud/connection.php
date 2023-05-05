1336051febd8317cc268597a362dd245d65c3f71
H A S A N <hasan25042019@gmail.com>
2023-05-05 12:04:54 +0600

    Create connection.php
    fdgf

---
+++ oopCrud/connection.php
@@ -0,0 +1,43 @@
+<?php
+    class db {
+        public $hostname;
+        public $username;
+        public $password;
+        public $db_name;
+        public $conn;
+    
+        public function __construct($hostname, $username, $password, $db_name) {
+            $this->hostname = $hostname;
+            $this->username = $username;
+            $this->password = $password;
+            $this->db_name = $db_name;
+            $this->conn = mysqli_connect($this->hostname, $this->username, $this->password, $this->db_name);
+    
+            if (!$this->conn) {
+                echo mysqli_connect_error();
+            } else {
+                echo "Connection Successful!";
+            }
+        }
+    
+        // public function insert() {
+        //     if (!$this->conn) {
+        //         echo "Connection failed";
+        //         return;
+        //     }
+    
+        //     $sqli = "INSERT INTO category(category_name, category_entrydate) VALUES('housing', '22-02-22')";
+        //     if(mysqli_query($this->conn, $sqli) == TRUE){
+        //         echo "Data Inserted";
+        //     }
+        //     else{
+        //         echo mysqli_error($this->conn);
+        //     }
+        // }
+    }
+    
+    $database = new db('localhost', 'root', '', 'store_db');
+    // $database->insert();
+    
+
+    
